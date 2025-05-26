3bd37b143a8e0008d1f8bc1a100e36d77b7eaf41
Praroop Trivedi <praroop.trivedi149@gmail.com>
2025-05-26 17:26:35 +0000

    datatype-summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,36 @@
+// Primitive
+
+// 7 Types :String,Number,Boolean,null,undefined,Symbol,BigInt
+//Java Script statically type or dynamically type
+
+const score=100
+const scoreValue=100.3
+
+const isLoggedIn=false
+const ouytSideTemp=null
+let userEmail;
+
+const id=Symbol('123')
+const anotherId=Symbol('123')
+// console.log(id===anotherId)
+
+const bigNumber=222222222222222222222222n
+// console.log(typeof bigNumber)
+
+
+
+//Non Primitive(Refrence Type)
+
+//Array,Object,Functions
+
+const heros=["Praroop","Rishab","Sangeeta"]
+let myObj={
+    name:"Praroop",
+    age:22,
+}
+
+const  myFunction=function(){
+    console.log("Hello Worled")
+}
+ console.log(typeof myFunction);
+ 
