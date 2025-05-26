ecc40038e13c637d11f94ae618e82bde46889f67
Praroop Trivedi <praroop.trivedi149@gmail.com>
2025-05-26 07:13:46 +0000

    01_variable

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,20 @@
+const accountId=14453 //Never Changed
+let accountEmail="Praroop.trivedi149@gmail.com"//
+var accountPassword="12345"
+accountCity="Delhi"
+
+let accountState
+
+
+// accountId=2 //Not allowed
+accountEmail="Trypbuddy@gmail.com"
+accountPassword="12332"
+accountCity="Bangluru"
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+/*
+prefrer not to use var 
+because of issue in block scope and functional scope 
+*/
