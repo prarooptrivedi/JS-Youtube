9c52edf9947f357b06a15ae89ed1395853f395d7
Praroop Trivedi <praroop.trivedi149@gmail.com>
2025-05-26 10:41:45 +0000

    conversionOperations

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,27 @@
+let score="true"
+// console.log(typeof(score))
+
+let valueInNumber=Number(score)
+// console.log(typeof(valueInNumber))
+
+// console.log(valueInNumber);
+
+//"33" =>33
+//"33abc"=>NaN
+//true=>1;false=>0
+
+//"=>false"
+// "hitesh"=>true
+
+let isLoggedIn=1
+
+let booleanIsLogdedIn=Boolean(isLoggedIn)
+// console.log(booleanIsLogdedIn)
+
+let someNumber=33
+let stringNumber=String(someNumber)
+console.log(stringNumber)
+
+console.log(typeof stringNumber)
+
+
