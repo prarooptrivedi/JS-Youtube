6844803309b763caa424a45a90261dfa6a5e8053
Praroop Trivedi <praroop.trivedi149@gmail.com>
2025-05-26 08:27:22 +0000

    Data Types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,18 @@
+let name="hitesh"
+let age=18
+let isLoggedIn=false
+
+
+
+// number =>2 to power 53
+// bigInt
+// string=>""
+// boolean =>true/false
+// null  =? standlon value
+/// undefined =>
+// symbol => unique
+
+//object 
+
+console.log(typeof undefined)// undefined
+console.log(typeof null) //object
