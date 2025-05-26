598d649b8130e087d8471a23a8daadc2c9d471ff
Praroop Trivedi <praroop.trivedi149@gmail.com>
2025-05-26 10:54:21 +0000

    prefixposrfix

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -20,8 +20,56 @@ let booleanIsLogdedIn=Boolean(isLoggedIn)
 
 let someNumber=33
 let stringNumber=String(someNumber)
-console.log(stringNumber)
+// console.log(stringNumber)
 
-console.log(typeof stringNumber)
+// console.log(typeof stringNumber)
+
+//*****************************Operations ************************** 
+
+let value=3
+let negValue=-value
+
+// console.log(negValue)
+
+
+// console.log(2+2);
+
+// console.log(2-2);
+
+// console.log(2*2);
+
+// console.log(2**2);
+
+// console.log(2/3);
+
+// console.log(2%2);
+
+let str1="hello"
+let str2="praroop"
+
+let str3=str1+str2
+// console.log(str3)
+
+// console.log("1"+2)
+// console.log(1+"2")
+// console.log(1+2+"2")
+
+// console.log(true+)
+
+let num1,num2,num3
+
+num1=num2=num3=2+2
+
+let gameCounter=100
+
+++gameCounter
+
+console.log(gameCounter)
+
+//Need To Learn Prefix and postFix operator
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+
+
+
 
 
