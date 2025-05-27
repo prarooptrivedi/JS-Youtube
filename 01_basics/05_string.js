74ad0d8e42d40ae91a0bd2ee6d689e66a0af973b
Praroop Trivedi <praroop.trivedi149@gmail.com>
2025-05-27 05:17:20 +0000

    String

---
+++ 01_basics/05_string.js
@@ -0,0 +1,32 @@
+const name="Praroop"
+const repoCount=50
+// console.log(name+repoCount+" Value")
+
+// console.log(`Hello my name is ${name
+// } and my repo coiunt is ${repoCount}`)
+
+
+const getName=new String('praroop-pc')//run in inspect
+
+// console.log(getName[0])
+// console.log(getName.toUpperCase())
+
+// console.log(getName.charAt(3))
+// console.log(getName.indexOf(`p`))
+
+const newString=getName.substring(0,4)
+// console.log(newString)
+
+const anotherString=getName.slice(-7,4)
+console.log(anotherString)
+
+const newStringOne="   hitesh     "
+
+console.log(newStringOne)
+console.log(newStringOne.trim())
+
+// console.log(newStringOne.replaceAll("h","H"))
+
+// console.log(newStringOne.includes("h"))
+
+console.log(newStringOne.split())
