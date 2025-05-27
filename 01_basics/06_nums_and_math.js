6d2027d77be3a3c3d8b7fc07965611b64b353010
Praroop Trivedi <praroop.trivedi149@gmail.com>
2025-05-27 05:45:21 +0000

    nums_and_math

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,50 @@
+const score=400
+
+// console.log(score)
+
+const balance=new Number(100)
+// console.log(balance)
+
+// console.log(balance.toString())
+// console.log(typeof balance.toString())
+
+// console.log(balance.toFixed(2))
+
+const otherNumber=123.69666
+
+// console.log(otherNumber.toPrecision(3))
+// console.log(otherNumber.toFixed(1))
+
+const hundred=1000000
+
+// console.log(hundred.toLocaleString('en-IN'))
+
+
+
+///+++++++++++++++++++++++++ Maths +++++++++++++++++++
+
+
+// console.log(Math.abs(-4))
+
+// console.log(Math.round(4.3))
+
+// console.log(Math.round(4.6))
+
+// console.log(Math.ceil(4.2))
+// console.log(Math.floor(4.9))
+
+// console.log(Math.min(4,3,6,1))
+
+// console.log(Math.max(3,5,6,1,5))
+
+// console.log(Math.random())
+
+// console.log(Math.random()*10)
+
+// console.log((Math.random()*10)+1)
+
+const min =10
+
+const max=20
+
+console.log(Math.floor(Math.random()*(max-min+1))+min)
