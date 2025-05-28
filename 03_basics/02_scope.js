4d31d45e43aba70dd8349d74e6cc83805afb2278
Praroop Trivedi <praroop.trivedi149@gmail.com>
2025-05-28 10:41:12 +0000

    Scope 2 vedio22

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -7,3 +7,31 @@ var c=30
 // console.log(a)
 // console.log(b)
  console.log(c)
+
+
+ function one(){
+    const userName="Praroop"
+    function two(){
+        const wesite="youyube"
+        console.log(userName)
+    }
+
+    // console.log(wesite)
+    two()
+ }
+ one()
+
+//============================== intresting =============================
+
+function addone(value){
+    return value+1
+}
+
+const result=addone(1)
+console.log(result)
+
+
+addtwo(2)
+const addtwo=function(num){
+    return num+2
+}
