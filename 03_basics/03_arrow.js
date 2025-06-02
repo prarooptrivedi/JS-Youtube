6993f366594b429cdaa549bec0129351862e006d
Praroop Trivedi <praroop.trivedi149@gmail.com>
2025-06-02 07:25:49 +0000

    arrow function

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -14,5 +14,43 @@ const user={
 // user.username="Sangeeta"
 // user.welcomeMessage()
 
-console.log(this)
+// console.log(this)
 
+// function chai(){
+//     let userName="Praroop"
+//     console.log(this.userName)
+// }
+// chai()
+
+// const chai = function(){
+//     let userName="Praroop"
+//     console.log(this.userName)
+// }
+
+// chai()
+
+
+// const chai=()=>{
+//     let username="praroop"
+//     console.log(this)
+// }
+// chai()
+
+// const addTwo=(num1,num2)=>{
+// return num1+num2
+// }
+
+// console.log(addTwo(3,4))
+
+
+// const addTwo=(num1,num2)=> num1+num2
+
+
+
+
+
+// const addTwo=(num1,num2)=>(num1+num2)//implicit return 
+
+const addTwo=(num1,num2)=>({username:"Praroop"})
+
+ console.log(addTwo(3,5))
